f21a21a1daff34e05140d8006a00d602a0f17313
0xayushM <mangal.ayush.4982@gmail.com>
2024-02-22 18:47:48 +0530

    cors error

--- src/main/java/com/ems/ems/EmsApplication.java
+++ src/main/java/com/ems/ems/EmsApplication.java
@@ -3,14 +3,15 @@ package com.ems.ems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
-//@PropertySource("classpath:.env")
+@EnableWebMvc
 public class EmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmsApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/ems/ems/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.ems.ems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://ems-frontend-djstz5ia7-0xayushm.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
+
